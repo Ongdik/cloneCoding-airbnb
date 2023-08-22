f011a293aafb421d5760104220157b547ec79ae9
dong-ik <ehddlr27@gmail.com>
2023-08-22 19:03:29 +0900

    laptop commit

--- main.js
+++ main.js
@@ -24,3 +24,50 @@ function onScroll() {
 window.addEventListener("scroll", () => {
   onScroll();
 });
+
+// const leftBtn = document.querySelector(".leftBtn");
+// const rightBtn = document.querySelector(".rightBtn");
+
+// function leftScroll() {
+
+// }
+
+// function rightScroll()
+
+document.addEventListener("DOMContentLoaded", function () {
+  const iconNav = document.querySelector(".iconNav");
+  const leftBtn = iconNav.querySelector(".leftBtn");
+  const rightBtn = iconNav.querySelector(".rightBtn");
+  const iconsList = iconNav.querySelector(".icons");
+
+  leftBtn.addEventListener("click", function () {
+    iconsList.scrollLeft -= 100; // 이동할 스크롤 양을 조절
+    updateButtonVisibility();
+  });
+
+  rightBtn.addEventListener("click", function () {
+    iconsList.scrollLeft += 100; // 이동할 스크롤 양을 조절
+    updateButtonVisibility();
+  });
+
+  function updateButtonVisibility() {
+    if (iconsList.scrollLeft === 0) {
+      leftBtn.style.display = "none";
+    } else {
+      leftBtn.style.display = "block";
+    }
+
+    if (
+      Math.ceil(iconsList.scrollLeft + iconNav.offsetWidth) >=
+      iconsList.scrollWidth
+    ) {
+      rightBtn.style.display = "none";
+    } else {
+      rightBtn.style.display = "block";
+    }
+  }
+
+  updateButtonVisibility();
+
+  window.addEventListener("resize", updateButtonVisibility);
+});
